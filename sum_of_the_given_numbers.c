350b3078420f882feab1e23e488c7168e07d59e8
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-02-12 17:55:54 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int arr[n][2],i,j;     for(i=0;i<n;i++)     {         for(j=0;j<2;j++)         {             scanf("%d",&arr[i][j]);
            }
            
        }
        for(i=0;i<n;i++)
        {
            for(j=0;j<2;j++)
            {
                sum=arr[i][j]+arr[i][j+1];
                break;
                
            }
            printf("%d
    ",sum);
            sum==0;
        }
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int arr[n][2],i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            
+        }
+        
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            sum=arr[i][j]+arr[i][j+1];
+            break;
+            
+        }
+        printf("%d
+",sum);
+        sum==0;
+    }
+}
