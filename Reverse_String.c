07f399f8ae75b4cf5a64b7fd09a9f71c1c7c3185
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-05-17 22:22:41 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     scanf("%[^ ]s",str);     int i,c=0;     for(i=0;str[i]!=NULL;i++)     {         c++;
        }
        for(i=c-1;i>=0;i--)
        {
            printf("%c",str[i]);
        }
        
    }

---
+++ Reverse_String.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    scanf("%[^
+]s",str);
+    int i,c=0;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        c++;
+        
+    }
+    for(i=c-1;i>=0;i--)
+    {
+        printf("%c",str[i]);
+    }
+    
+}
