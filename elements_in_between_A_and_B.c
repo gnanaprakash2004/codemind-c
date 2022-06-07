0f6c4dd1cdfdf571171fa7ec2b79c3b7a807527d
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-06-07 11:31:53 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b;     scanf("%d%d",&a,&b);     int min=0;     int c=0;     for(int i=0;i<n;i++)     {         if(arr[i]>=a&&arr[i]<=b)         {             printf("%d ",arr[i]);             c=1;         }
        }
    if(c==0)
    
    {
        printf("-1");
    }
       
    }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int min=0;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>=a&&arr[i]<=b)
+        {
+            printf("%d ",arr[i]);
+            c=1;
+        }
+        
+    }
+if(c==0)
+
+{
+    printf("-1");
+}
+   
+}
