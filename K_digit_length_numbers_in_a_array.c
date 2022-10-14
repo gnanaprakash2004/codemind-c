5c37e7406d2e00cb409849bd5f5ec9c76ec3dba6
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-14 19:30:54 +0530

    #include<stdio.h> int main()     {         int n,a[100],i,j,r,c,k,d=0;         scanf("%d%d",&n,&k);         for(i=0;i<n;i++)         {             scanf("%d",&a[i]);         }         for(i=0;i<n;i++)         { c=0;           if(a[i]==0)           {               c=1;           }           else           {             while(a[i]!=0)             {  r=a[i]%10;                 c=c+1;                 a[i]=a[i]/10;             }             //printf("%d ",c);           }           if(c==k)           {               d++;           }         }         printf("%d",d);     }

--- K_digit_length_numbers_in_a_array.c
+++ K_digit_length_numbers_in_a_array.c
@@ -1,35 +1,31 @@
 #include<stdio.h>
-int co(int n)
-{
-        int c=0;
-    if (n==0)
-    {
-        return 1;
-    }
-    if (n<0)
-    {
-        n*=-1;
-    }
-    while (n>0)
-    {
-        c++;
-        n/=10;
-    }
-    return c;
-}
 int main()
-{
-    int n,k;
-    scanf("%d%d",&n,&k);
-    int arr[n];
-    int count=0;
-    for(int i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
-        if(co(arr[i])==k)
+        int n,a[100],i,j,r,c,k,d=0;
+        scanf("%d%d",&n,&k);
+        for(i=0;i<n;i++)
         {
-            count++;
+            scanf("%d",&a[i]);
         }
+        for(i=0;i<n;i++)
+        { c=0;
+          if(a[i]==0)
+          {
+              c=1;
+          }
+          else
+          {
+            while(a[i]!=0)
+            {  r=a[i]%10;
+                c=c+1;
+                a[i]=a[i]/10;
+            }
+            //printf("%d ",c);
+          }
+          if(c==k)
+          {
+              d++;
+          }
+        }
+        printf("%d",d);
     }
-    printf("%d",count);
-}
