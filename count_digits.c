dad44719492bf4a37c88438bdbc2b3f260db3ae5
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-14 19:30:59 +0530

    #include<stdio.h> int main()     {         int n,a[100],i,j,r,c;         scanf("%d",&n);         for(i=0;i<n;i++)         {             scanf("%d",&a[i]);         }         for(i=0;i<n;i++)         { c=0;           if(a[i]==0)           {               printf("1 ");               continue;           }             while(a[i]!=0)             {  r=a[i]%10;                 c=c+1;                 a[i]=a[i]/10;             }             printf("%d ",c);         }     }

--- count_digits.c
+++ count_digits.c
@@ -1,33 +1,24 @@
 #include<stdio.h>
-int co(int n)
-{
-        int c=0;
-    if (n==0)
-    {
-        return 1;
-    }
-    if (n<0)
-    {
-        n*=-1;
-    }
-    while (n>0)
-    {
-        c++;
-        n/=10;
-    }
-    return c;
-}
 int main()
-{
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        int n,a[100],i,j,r,c;
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++)
+        { c=0;
+          if(a[i]==0)
+          {
+              printf("1 ");
+              continue;
+          }
+            while(a[i]!=0)
+            {  r=a[i]%10;
+                c=c+1;
+                a[i]=a[i]/10;
+            }
+            printf("%d ",c);
+        }
     }
-    for(int i=0;i<n;i++)
-    {
-        printf("%d ",co(arr[i]));
-    }
-}
