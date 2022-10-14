72c2b0ece8d86b3f47a1c608fa7be2181c488962
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-14 23:43:50 +0530

    #include<stdio.h> int main() {     int n,a[100],s[100];     scanf("%d",&n);     int i,sum=0,x,b,j=0,max=0;
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
        scanf("%d %d",&x,&b);
        for(i=0;i<n;i++)
        {
            if(a[i]<x)
            {
                s[j]=a[i];
                j++;
            }
            else if(a[i]>x && a[i]>b)
            {
                s[j]=a[i];
                j++;
            }
            else if (a[i]==x || a[i]==b)
            {
                continue;
            }
        }
        if(j==0)
        {
            printf("-1");
            return 0;
        }
        else
        { max=s[0];
        for(i=1;i<j;i++)
        {
            if(s[i]>max)
            {
                max=s[i];
            }
        }
        if(max==0)
        {
            printf("-1");
        }
        else
        {
            printf("%d",max);
        }
        }
    }

--- maximum_of_numbers_not_between_A_and_B.c
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -1,25 +1,53 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,a[100],s[100];
     scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
+    int i,sum=0,x,b,j=0,max=0;
+    
+    for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
-    int x,y;
-    scanf("%d%d",&x,&y);
-    int m=-1;
-    for(int i=0;i<n;i++)
+    scanf("%d %d",&x,&b);
+    for(i=0;i<n;i++)
     {
-        if(arr[i]<x||arr[i]>y)
+        if(a[i]<x)
         {
-            if(arr[i]>m)
-            {
-                m=arr[i];
-            }
+            s[j]=a[i];
+            j++;
+        }
+        else if(a[i]>x && a[i]>b)
+        {
+            s[j]=a[i];
+            j++;
+        }
+        else if (a[i]==x || a[i]==b)
+        {
+            continue;
         }
     }
-    printf("%d",m);
+    if(j==0)
+    {
+        printf("-1");
+        return 0;
+    }
+    else
+    { max=s[0];
+    for(i=1;i<j;i++)
+    {
+        if(s[i]>max)
+        {
+            max=s[i];
+        }
+    }
+    if(max==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",max);
+    }
+    }
 }
