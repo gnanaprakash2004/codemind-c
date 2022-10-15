07c8122b8e350d3936ac0003685b179f912fc628
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-16 00:45:39 +0530

    #include<stdio.h> int main() {     int n,m,i,q,s,res=0,re;     scanf("%d",&m);     if(m>=10 && m<=10000)     {         for(n=m+1;n<=100000;n++)         {             s=1;             for(i=2;i<n/2;i++)             {                 if(n%i==0)                 {                     s=0;                     break;                 }             }             if(s==1)             {                 res=0;                 q=n;                 while(q!=0)                 {                     re=q%10;                     res=(res*10)+re;                     q=q/10;                 }                 if(res==n)                 {                     printf("%d",n);                     break;                 }             }         }     } }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,34 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int m,n,i,rem,res,q,s;
+    int n,m,i,q,s,res=0,re;
     scanf("%d",&m);
-    for(n=m+1;n<=100000;n++)
+    if(m>=10 && m<=10000)
     {
-        s=1;
-        for(i=2;i<=n/2;i++)
+        for(n=m+1;n<=100000;n++)
         {
-            if(n%i==0)
+            s=1;
+            for(i=2;i<n/2;i++)
             {
-                s=0;
-                break;
+                if(n%i==0)
+                {
+                    s=0;
+                    break;
+                }
             }
-        }
-        if(s==1)
-        {
-            res=0;
-            q=n;
-            while(q>0)
+            if(s==1)
             {
-                rem=q%10;
-                res=(res*10)+rem;
-                q=q/10;
-            
-            }
-            if(res==n)
-            {
-                printf("%d",n);
-                break;
+                res=0;
+                q=n;
+                while(q!=0)
+                {
+                    re=q%10;
+                    res=(res*10)+re;
+                    q=q/10;
+                }
+                if(res==n)
+                {
+                    printf("%d",n);
+                    break;
+                }
             }
         }
     }
