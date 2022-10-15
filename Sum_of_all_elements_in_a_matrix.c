bfb8f53888a947d8098ce66c723f56272635981f
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-16 00:23:33 +0530

    #include<stdio.h> int main() {     int n,m,a[10][10],i,j,s=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             s+=a[i][j];         }     }     printf("%d",s); }

--- Sum_of_all_elements_in_a_matrix.c
+++ Sum_of_all_elements_in_a_matrix.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,i,j;
+    int n,m,a[10][10],i,j,s=0;
     scanf("%d%d",&n,&m);
-    int a[n][m];
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
@@ -11,7 +10,6 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
-    int s=0;
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
@@ -20,6 +18,4 @@ int main()
         }
     }
     printf("%d",s);
-
-
 }
